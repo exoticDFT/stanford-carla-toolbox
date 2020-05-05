eb2b979be587a6e9ea71202a575e5f30a6c03196
Alexander Koufos <exoticdft@gmail.com>
2020-05-04 18:49:31 -0700

    Renames the Julia source files
    - Renames the Julia source files to better represent their overall functionality and remove the old ADM notation.
    - Updates the exports within main Julia source file to use the new filenames.
    - Updates the comments in the main Julia source file.
    - Updates the formatting of the main Julia source file.

--- src/StanfordCarlaToolbox.jl
+++ src/StanfordCarlaToolbox.jl
@@ -4,20 +4,20 @@ import AutomotiveSimulator
 import Formatting
 import PyCall
 
-# AutomotiveSimulator records based functions
-include("adm_records.jl")
+# AutomotiveSimulator based functions
+include("autosim_utils.jl")
 export current_world_to_scene,
        actor_to_entity,
        get_entity_scene
 
-# Carla scenario generation related functions
-include("adm_scenario.jl")
-export destroy_actors,
-       initiate_scenario
-
-# AutomotiveSimulator simulation based functions
-include("adm_sim.jl")
+# Carla client based functions
+include("client_utils.jl")
 export create_carla_client,
        get_server_information
+
+# Carla scenario generation related functions
+include("scenario_utils.jl")
+export destroy_actors,
+       initiate_scenario
 
 end #StanfordCarlaToolbox
